79700b1dc8e256a6d92ee971229af8292cff841f
DanyaBooba <danil.dybko@gmail.com>
2022-02-27 19:37:08 +0300

    add FAQ

--- Scripts/Menu/OpenURL.cs
+++ Scripts/Menu/OpenURL.cs
@@ -6,7 +6,9 @@ public class OpenURL : MonoBehaviour
 {
     private string urlSite = "https://creagoo.ru";
     private string urlGooglePlay = "https://live.creagoo.ru/assets/php/content/url-conversion.php?w=googleplay";
-
+    private string urlWeb = "https://live.creagoo.ru/games/babkaonthehuntclassic/online";
+    private string urlItchio = "https://live.creagoo.ru/assets/php/content/url-conversion.php?w=itchio";
+    
     public void OpenSite()
     {
         URL(urlSite);
@@ -20,6 +22,16 @@ public class OpenURL : MonoBehaviour
     public void GetOpenURL(string url)
     {
         URL(url);
+    }
+
+    public void OpenWeb()
+    {
+        URL(urlWeb);
+    }
+
+    public void OpenItchio()
+    {
+        URL(urlItchio);
     }
 
     private void URL(string url)
